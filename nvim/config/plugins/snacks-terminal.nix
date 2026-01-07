2f78615844518024815c456da1efb98dfc79fe3e
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2026-01-07 22:10:24 +0100

    fix: nvim terminal mode navigation

--- nvim/config/plugins/snacks-terminal.nix
+++ nvim/config/plugins/snacks-terminal.nix
@@ -11,7 +11,7 @@
             nav_h = {
               __unkeyed-1 = "<C-h>";
               __unkeyed-2.__raw =
-                ''function() vim.api.nvim_command("wincmd h") end'';
+                ''function() return "<C-\\><C-n><C-w>h" end'';
               desc = "Go to Left Window";
               expr = true;
               mode = "t";
@@ -19,7 +19,7 @@
             nav_j = {
               __unkeyed-1 = "<C-j>";
               __unkeyed-2.__raw =
-                ''function() vim.api.nvim_command("wincmd j") end'';
+                ''function() return "<C-\\><C-n><C-w>j" end'';
               desc = "Go to Lower Window";
               expr = true;
               mode = "t";
@@ -27,7 +27,7 @@
             nav_k = {
               __unkeyed-1 = "<C-k>";
               __unkeyed-2.__raw =
-                ''function() vim.api.nvim_command("wincmd k") end'';
+                ''function() return "<C-\\><C-n><C-w>k" end'';
               desc = "Go to Upper Window";
               expr = true;
               mode = "t";
@@ -35,7 +35,7 @@
             nav_l = {
               __unkeyed-1 = "<C-l>";
               __unkeyed-2.__raw =
-                ''function() vim.api.nvim_command("wincmd l") end'';
+                ''function() return "<C-\\><C-n><C-w>l" end'';
               desc = "Go to Right Window";
               expr = true;
               mode = "t";
@@ -59,14 +59,20 @@
         options.desc = "Terminal (Project Root)";
       }
       {
-        mode = [ "n" "t" ];
+        mode = [ "n" ];
         key = "<c-/>";
         action.__raw =
           "function() Snacks.terminal(nil, { cwd = Snacks.git.get_root() }) end";
         options.desc = "Terminal (Root Dir)";
       }
       {
-        mode = [ "n" "t" ];
+        mode = [ "t" ];
+        key = "<c-_>";
+        action = "<C-\\><C-n><Cmd>close<CR>";
+        options.desc = "Hide terminal";
+      }
+      {
+        mode = [ "n" ];
         key = "<c-_>";
         action.__raw =
           "function() Snacks.terminal(nil, { cwd = Snacks.git.get_root() }) end";
