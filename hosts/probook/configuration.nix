ca39bd84daa1a7a59930f1577bc46b2ab208c396
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2026-01-08 10:49:35 +0100

    feature: add seperate configuration for hp probook

---
+++ hosts/probook/configuration.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  imports = [
+    ./disko.nix
+    ../../modules/secrets.nix
+    ../../modules/desktop
+    ../../modules/storage/zfs.nix
+    ../../modules/users/luuk/desktop.nix
+  ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  system.stateVersion = "25.11";
+
+  networking.hostName = "probook";
+  networking.hostId = "6bbc35ad";
+}
