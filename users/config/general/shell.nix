ef8f0561ed8d8c2a416826689da1e42bf3073d64
Luuk Blankenstijn <me@luukblankenstijn.nl>
2026-01-08 17:31:25 +0100

    feature: enable autosuggestions

--- users/config/general/shell.nix
+++ users/config/general/shell.nix
@@ -1,49 +1,56 @@
-{ ... }: {
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-    options = [ "--cmd cd" ];
-  };
-
-  programs.direnv = {
-    enable = true;
-    enableZshIntegration = true;
-    nix-direnv = { enable = true; };
-    silent = true;
-  };
-
-  programs.oh-my-posh = {
-    enable = true;
-    enableZshIntegration = true;
-    useTheme = "multiverse-neon";
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    history = {
-      path = "$HOME/.zsh_history";
-      size = 100000;
-      save = 100000;
-      share = true;
-      ignoreDups = true;
-      ignoreSpace = true;
-      extended = true;
-    };
-    syntaxHighlighting.enable = true;
-    antidote = {
+_: {
+  programs = {
+    zoxide = {
       enable = true;
-      plugins = [
-        "getantidote/use-omz"
-        "ohmyzsh/ohmyzsh path:lib"
-
-        "ohmyzsh/ohmyzsh path:themes/robbyrussell.zsh-theme"
-
-        "ohmyzsh/ohmyzsh path:plugins/git"
-        "ohmyzsh/ohmyzsh path:plugins/sudo"
-        "ohmyzsh/ohmyzsh path:plugins/ssh-agent"
-      ];
+      enableZshIntegration = true;
+      options = [ "--cmd cd" ];
     };
-    shellAliases = { ls = "ls -Ahl"; };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv = {
+        enable = true;
+      };
+      silent = true;
+    };
+
+    oh-my-posh = {
+      enable = true;
+      enableZshIntegration = true;
+      useTheme = "multiverse-neon";
+    };
+
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      enableCompletion = true;
+      history = {
+        path = "$HOME/.zsh_history";
+        size = 100000;
+        save = 100000;
+        share = true;
+        ignoreDups = true;
+        ignoreSpace = true;
+        extended = true;
+      };
+      syntaxHighlighting.enable = true;
+      antidote = {
+        enable = true;
+        plugins = [
+          "getantidote/use-omz"
+          "ohmyzsh/ohmyzsh path:lib"
+
+          "ohmyzsh/ohmyzsh path:themes/robbyrussell.zsh-theme"
+
+          "ohmyzsh/ohmyzsh path:plugins/git"
+          "ohmyzsh/ohmyzsh path:plugins/sudo"
+          "ohmyzsh/ohmyzsh path:plugins/ssh-agent"
+        ];
+      };
+      shellAliases = {
+        ls = "ls -Ahl";
+      };
+    };
   };
 }
