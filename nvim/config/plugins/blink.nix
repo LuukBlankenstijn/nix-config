2a25d3445320c34e22a09346b32b17517c1a6d7f
Luuk Blankenstijn <me@luukblankenstijn.nl>
2026-01-08 21:23:08 +0100

    feature(nvim): keymap to accept blink suggestion

--- nvim/config/plugins/blink.nix
+++ nvim/config/plugins/blink.nix
@@ -3,18 +3,52 @@
     blink-cmp = {
       enable = true;
       settings = {
-        keymap.preset = "default";
+        keymap = {
+          preset = "default";
+
+          "<CR>" = [
+            "accept"
+            "fallback"
+          ];
+
+          "<Tab>" = [
+            "select_next"
+            "fallback"
+          ];
+          "<S-Tab>" = [
+            "select_prev"
+            "fallback"
+          ];
+
+          "<C-b>" = [
+            "scroll_documentation_up"
+            "fallback"
+          ];
+          "<C-f>" = [
+            "scroll_documentation_down"
+            "fallback"
+          ];
+        };
         appearance = {
           use_nvim_cmp_as_default = true;
           nerd_font_variant = "mono";
         };
-        sources = { default = [ "lsp" "path" "snippets" "buffer" ]; };
+        sources = {
+          default = [
+            "lsp"
+            "path"
+            "snippets"
+            "buffer"
+          ];
+        };
       };
     };
 
     blink-compat = {
       enable = true;
-      settings = { impersonate_nvim_cmp = true; };
+      settings = {
+        impersonate_nvim_cmp = true;
+      };
     };
 
     luasnip.enable = true;
