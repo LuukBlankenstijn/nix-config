70e4dd739cd2ffdeb580174d61f9aa533d3dbd41
Luuk Blankenstijn <me@luukblankenstijn.nl>
2026-01-11 14:59:45 +0100

    add prism launcher

--- users/config/general/default.nix
+++ users/config/general/default.nix
@@ -1,5 +1,14 @@
-{ pkgs, ... }: {
-  imports = [ ./git.nix ./nvim.nix ./ranger.nix ./shell.nix ];
+{ pkgs, ... }:
+{
+  imports = [
+    ./git.nix
+    ./nvim.nix
+    ./ranger.nix
+    ./shell.nix
+  ];
 
-  home.packages = with pkgs; [ codex ];
+  home.packages = with pkgs; [
+    codex
+    prismlauncher
+  ];
 }
