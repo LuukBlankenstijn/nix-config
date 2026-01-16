fc220dd6e7d3e77f53a04ca7d3fad1ffd1e30c2f
Luuk Blankenstijn <me@luukblankenstijn.nl>
2026-01-16 18:55:14 +0100

    nvim terminal buffer not title

--- nvim/config/plugins/snacks-terminal.nix
+++ nvim/config/plugins/snacks-terminal.nix
@@ -2,39 +2,41 @@ _: {
   config = {
     plugins.snacks = {
       settings = {
-        bigfile.enabled = true;
-        quickfile.enabled = true;
-        profiler.enabled = true;
         terminal = {
           enabled = true;
-          win.keys = {
-            nav_h = {
-              __unkeyed-1 = "<C-h>";
-              __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>h" end'';
-              desc = "Go to Left Window";
-              expr = true;
-              mode = "t";
+          win = {
+            wo = {
+              winbar = "";
             };
-            nav_j = {
-              __unkeyed-1 = "<C-j>";
-              __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>j" end'';
-              desc = "Go to Lower Window";
-              expr = true;
-              mode = "t";
-            };
-            nav_k = {
-              __unkeyed-1 = "<C-k>";
-              __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>k" end'';
-              desc = "Go to Upper Window";
-              expr = true;
-              mode = "t";
-            };
-            nav_l = {
-              __unkeyed-1 = "<C-l>";
-              __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>l" end'';
-              desc = "Go to Right Window";
-              expr = true;
-              mode = "t";
+            keys = {
+              nav_h = {
+                __unkeyed-1 = "<C-h>";
+                __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>h" end'';
+                desc = "Go to Left Window";
+                expr = true;
+                mode = "t";
+              };
+              nav_j = {
+                __unkeyed-1 = "<C-j>";
+                __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>j" end'';
+                desc = "Go to Lower Window";
+                expr = true;
+                mode = "t";
+              };
+              nav_k = {
+                __unkeyed-1 = "<C-k>";
+                __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>k" end'';
+                desc = "Go to Upper Window";
+                expr = true;
+                mode = "t";
+              };
+              nav_l = {
+                __unkeyed-1 = "<C-l>";
+                __unkeyed-2.__raw = ''function() return "<C-\\><C-n><C-w>l" end'';
+                desc = "Go to Right Window";
+                expr = true;
+                mode = "t";
+              };
             };
           };
         };
