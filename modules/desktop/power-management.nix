8989dec87247bff6b7ed675205f87786a04349df
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-02-16 11:42:33 +0100

    enable powersave always

--- modules/desktop/power-management.nix
+++ modules/desktop/power-management.nix
@@ -5,16 +5,16 @@ _: {
       STOP_CHARGE_THRESH_BAT0 = 80;
       START_CHARGE_THRESH_BAT0 = 75;
 
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_AC = 0;
       CPU_BOOST_ON_BAT = 0;
 
-      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_AC = "low-power";
       PLATFORM_PROFILE_ON_BAT = "low-power";
 
       USB_AUTOSUSPEND = 1;
