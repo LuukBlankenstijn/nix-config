f5d5b5e1a7df5c76df4bd8c5cdab36b2beec0c79
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-03-30 17:22:57 +0200

    install protonmail-desktop

--- users/config/gui/thunderbird.nix
+++ users/config/gui/thunderbird.nix
@@ -1,8 +1,12 @@
-_: {
+{ pkgs, ... }:
+{
   programs.thunderbird = {
     enable = true;
     profiles.default = {
       isDefault = true;
     };
   };
+  home.packages = with pkgs; [
+    protonmail-desktop
+  ];
 }
